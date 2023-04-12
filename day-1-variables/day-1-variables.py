16af5678be79a8487f9401f7e8c644506286c7e4
Cristian Albu <albucristian95p@gmail.com>
2023-04-12 12:16:12 +0300

    day 1 complete

--- day-1-variables/day-1-variables.py
+++ day-1-variables/day-1-variables.py
@@ -1,1 +1,29 @@
 print("Hello world\nHello world")
+
+print("Hello" + " " + "World")
+
+print("Hello "+ input("What is your name? "))
+
+print(len(input("What is your name? ")))
+
+name = input("What is your name? ")
+print(name)
+
+
+a = input("a: ")
+b = input("b: ")
+print(a, b)
+
+c = a
+a = b
+b = c
+
+print("a=" + a + " b= " + b + " c=" + c)
+
+# Day 1 project
+
+city = input("What city did you grow up in? \n")
+
+pet = input("What was your pet? \n")
+
+print("Your band name is: " + city + " " + pet)
