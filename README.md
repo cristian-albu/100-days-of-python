959a152b9138a15406315b69ead48eb5bfc5c2ff
cristian-albu <albucristian95p@gmail.com>
2023-11-03 15:02:54 +0200

    guess number game

--- README.md
+++ README.md
@@ -10,14 +10,14 @@ Master Python by building 100 projects in 100 days. Learn data science, automati
 -   [x] Day 2
 -   [x] Day 3
 -   [x] Day 4
--   [ ] Day 5
--   [ ] Day 6
--   [ ] Day 7
--   [ ] Day 8
--   [ ] Day 9
--   [ ] Day 10
--   [ ] Day 11
--   [ ] Day 12
+-   [x] Day 5
+-   [x] Day 6
+-   [x] Day 7
+-   [x] Day 8
+-   [x] Day 9
+-   [x] Day 10
+-   [x] Day 11
+-   [x] Day 12
 -   [ ] Day 13
 -   [ ] Day 14
 -   [ ] Day 15
---
+++ day-12-number-guessing-game/main.py
@@ -0,0 +1,43 @@
+import random
+
+def init_game():
+    difficulty = input("Choose difficulty. Type 'easy' or 'hard': ")
+    if difficulty != 'easy' and difficulty != 'hard':
+        print('You have to choose a difficulty')
+        init_game()
+    else:
+        return difficulty
+    
+def guess_number():
+    guess = int(input('Guess a number: '))
+    if guess == number:
+        print('You win')
+        return True
+    elif guess > number:
+        print('Too high')
+        return False
+    elif guess < number:
+        print('Too low')
+        return False
+
+print("I'm thinking of a number between 1 and 100")
+number = random.choice(range(1, 101))
+lives = 5
+difficulty = init_game()
+
+if difficulty == 'easy':
+    lives += 10
+
+while lives > 0:
+    print(f"Lives left: {lives}")
+
+    guess = guess_number()
+    if guess == True:
+        break
+    else:
+        lives -= 1
+        
+
+
+
+
